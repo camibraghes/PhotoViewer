a1f72b8ab13d9e11d57070822202e273cc1480fe
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-15 14:55:35 +0200

    List images with FileManager

--- PhotoViewer/ViewController.swift
+++ PhotoViewer/ViewController.swift
@@ -1,10 +1,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var picture = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                picture.append(item)
+            }
+        }
+        print(picture)
     }
 
 
