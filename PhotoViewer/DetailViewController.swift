d6e3c824748b831b582aa4a82b09c8197b944723
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-15 17:10:12 +0200

    Add image view

--- PhotoViewer/DetailViewController.swift
+++ PhotoViewer/DetailViewController.swift
@@ -2,12 +2,13 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
